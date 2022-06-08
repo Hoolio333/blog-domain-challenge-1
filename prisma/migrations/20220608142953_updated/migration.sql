abd2479d3417f61879bbeeb54a3fccaf1f937ab9
Chris Hyde <c_hyde333@hotmail.com>
2022-06-08 15:35:12 +0100

    current stage

---
+++ prisma/migrations/20220608142953_updated/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "userId" INTEGER;
+
+-- AlterTable
+ALTER TABLE "Profile" ADD COLUMN     "userId" INTEGER;
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/seed.js
+++ prisma/seed.js
@@ -11,6 +11,32 @@ async function seed() {
 
   console.log(`${createdUsers.count} users created`, createdUsers);
 
+  const createdProfile = await prisma.profile.createMany({
+    data: [
+      {
+        profilePic: "https://i.gyazo.com/a25f1b0d00eabea1e20234f31a5049a8.png",
+        biography: "I'm the greatest profile ever!",
+      },
+      {
+        profilePic: "https://i.gyazo.com/4010cbe3c6cd7f47fb050d9364057ab0.png",
+        biography: "I'm the second greatest profile ever!",
+      },
+    ],
+  });
+
+  const createdPost = await prisma.post.createMany({
+    data: [
+      {
+        title: "I did it My Way",
+        content: "Frank S would be proud...",
+      },
+      {
+        title: "Why did the chicken...",
+        content: "Cross the road?",
+      },
+    ],
+  });
+
   // Add your code here
 
   // Don't edit any of the code below this line

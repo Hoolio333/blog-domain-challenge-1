d66105b340e5fc64e6854824b45bd167a581c355
Chris Hyde <c_hyde333@hotmail.com>
2022-06-08 17:54:26 +0100

    Extension - done

--- prisma/seed.js
+++ prisma/seed.js
@@ -14,10 +14,12 @@ async function seed() {
   const createdProfile = await prisma.profile.createMany({
     data: [
       {
+        userId: 1,
         profilePic: "https://i.gyazo.com/a25f1b0d00eabea1e20234f31a5049a8.png",
         biography: "I'm the greatest profile ever!",
       },
       {
+        userId: 2,
         profilePic: "https://i.gyazo.com/4010cbe3c6cd7f47fb050d9364057ab0.png",
         biography: "I'm the second greatest profile ever!",
       },
@@ -27,12 +29,31 @@ async function seed() {
   const createdPost = await prisma.post.createMany({
     data: [
       {
+        userId: 1,
         title: "I did it My Way",
         content: "Frank S would be proud...",
       },
       {
+        userId: 2,
         title: "Why did the chicken...",
         content: "Cross the road?",
+      },
+    ],
+  });
+
+  const createdComment = await prisma.comment.createMany({
+    data: [
+      {
+        userId: 1,
+        comment: "Jack and Jill...",
+        postId: 1,
+        commentId: 1,
+      },
+      {
+        userId: 2,
+        comment: "Went up the hill...",
+        postId: 2,
+        commentId: 2,
       },
     ],
   });
